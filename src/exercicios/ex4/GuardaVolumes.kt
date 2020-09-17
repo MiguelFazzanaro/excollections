b6d16a720cb0f86a05da2620691a7e11eb0a978b
MiguelFazzanaro <mfazzanaro@gmail.com>
2020-09-17 15:38:37 -0300

    ajustes finais

--- src/exercicios/ex4/GuardaVolumes.kt
+++ src/exercicios/ex4/GuardaVolumes.kt
@@ -1,23 +1,23 @@
 package exercicios.ex4
 
 class GuardaVolumes {
-    var contador = 1
-    val dicionario = mutableMapOf(contador,Pecas)
+/* var contador = 1
+val dicionario = mutableMapOf(contador,Pecas)
 
-    override fun retirada() {
-        TODO("Not yet implemented")
-    }
-    fun guardarPecas(roupas: List<Pecas>){
-        dicionario.put(contador,roupas)
-        contador ++
-    }
-    fun mostrarPecas(){
+ //override fun retirada() {
+     TODO("Not yet implemented")
+ }
+ fun guardarPecas(roupas: List<Pecas>){
+     dicionario.put(contador,roupas)
+     contador ++
+ }
+ fun mostrarPecas(){
 
-    }
-    fun mostrarPecas(Integernumero){
+ }
+ fun mostrarPecas(Integernumero){
 
-    }
-    fun devolverPecas(Integernumero){
+ }
+ fun devolverPecas(Integernumero){
 
-    }
+ }*/
 }
